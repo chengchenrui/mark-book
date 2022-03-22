c5cfad86e19ea5ea8b1d7486c8533867cc72c449
chengchenrui <chengchenr@qq.com>
2022-03-22 17:01:47 +0800

    add

---
+++ src/com/cheng/plugin/markbook/window/NoteListWindow.java
@@ -0,0 +1,13 @@
+package com.cheng.plugin.markbook.window;
+
+import javax.swing.*;
+
+public class NoteListWindow {
+    private JPanel     contentPanel;
+    private JTextField tfTopic;
+    private JTable     tableContent;
+    private JButton    buttonCreate;
+    private JButton    buttonClear;
+    private JButton    buttonClose;
+
+}
